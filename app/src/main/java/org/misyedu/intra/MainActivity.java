376487ad0e8d6b4ba212ab47b93ad33c3a7afe07
Tun Win Lat <tunwinlatt@windowslive.com>
2019-11-07 08:53:29 +0630

    Initial Commit

---
+++ app/src/main/java/org/misyedu/intra/MainActivity.java
@@ -0,0 +1,46 @@
+package org.misyedu.intra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView =(WebView)findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://sites.google.com/misyedu.org/dashboard?pli=1");
+        Button button = (Button) findViewById(R.id.refreshButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                webView.reload();
+
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
